bde9616062115d630f3a58bf58bef994ee543a50
Krystof Langpaul <kysilangp@gmail.com>
2025-07-21 14:23:16 +0200

    new name to outpout

--- ase.py
+++ ase.py
@@ -1,5 +1,5 @@
 f=open("main.asm","r")
-f2=open("program.bin","wb")
+f2=open("flash.bin","wb")
 ins=["ADD","SUB","REGWRITE","REGMOV","RAMW","RAMR","JMP","JPIE","JPIG","JPIS","INC","DEC","NOP\n","JPINE","BITSLEFT","HALT\n","BITSRIGHT","CALL","RET"]
 buff=""
 buffc1=""
